a0350a6aa04f5cdc81e5aa6e3e87ac81cd6b8e33
thatpersoninthemirror <ryan.bishop258@gmail.com>
2020-02-17 09:48:15 -0600

    created TacoRepository file

---
+++ LoggingKata/ITacoRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoggingKata
+{
+    interface ITacoRepository
+    {
+        void InsertTacoBell();
+        IEnumerable<TacoBell> GetTacoBells();
+    }
+}
---
+++ LoggingKata/TacoRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using Dapper;
+
+namespace LoggingKata
+{
+    class TacoRepository : ITacoRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public TacoRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+        public IEnumerable<TacoBell> GetTacoBells()
+        {
+            return _connection.Query<TacoBell>("SELECT * FROM tacobells;").ToList();
+        }
+
+        public void InsertTacoBell(double latitude, double longitude, string taconame)
+        {
+            _connection.Execute("INSERT INTO tacobells (latitude, longitude, name) VALUES (@lat, @lon, @name)", new { lat = latitude, lon = longitude, name = taconame });
+        }
+    }
+}
